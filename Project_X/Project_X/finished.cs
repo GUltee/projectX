36cac8e27e73c903ac12c248625fc6db9c0e87f8
Alec <alecguy@gmail.com>
2018-12-18 15:33:51 +0100

    anothaone

---
+++ Project_X/Project_X/finished.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Project_X
+{
+    public partial class finished : UIViewController
+    {
+        public finished (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ Project_X/Project_X/no_data.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Project_X
+{
+    public partial class no_data : UIViewController
+    {
+        public no_data (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
